a18bd854ecefa76d5be4f3a79e21225c5c49a50d
Alexey Raga <alexey.raga@gmail.com>
2025-12-08 08:54:16 +1100

    Add auto GC

--- darwin-configuration.nix
+++ darwin-configuration.nix
@@ -53,6 +53,11 @@ in
     # package = pkgs.nix;
     package = pkgs.nixVersions.latest;
 
+    gc = {
+      automatic = true;
+      options = "--delete-older-than 60d";
+    };
+
     settings = {
       experimental-features = ["nix-command" "flakes"];
       
