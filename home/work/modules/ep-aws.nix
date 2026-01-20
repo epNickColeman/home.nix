fbd8a7d17f83877e042413df79563f05b88b8fbb
Nick Coleman <nick.coleman@educationperfect.com>
2026-01-20 18:44:25 +1300

    Pass parameters to epup script

--- home/work/modules/ep-aws.nix
+++ home/work/modules/ep-aws.nix
@@ -18,7 +18,7 @@ let
     fi
     aws ecr get-login-password --region ap-southeast-2 --profile ecr | docker login --username AWS --password-stdin 058337015204.dkr.ecr.ap-southeast-2.amazonaws.com
 
-    docker compose up -d
+    docker compose up -d "$@"
   '';
 
   epLogin = pkgs.writeShellScriptBin "eplogin" "aws sso login --sso-session ep";
