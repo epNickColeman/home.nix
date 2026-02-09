bef0e357973280ade9ce92212f894c6cd5f90f8f
Nick Coleman <nick.coleman@educationperfect.com>
2026-02-09 16:33:39 +1300

    Fix hashes

--- overlays/nushell-plugns/semver.nix
+++ overlays/nushell-plugns/semver.nix
@@ -1,16 +1,16 @@
 self: super: {
   nushellPlugins = super.nushellPlugins // {
     semver = super.nushellPlugins.semver.overrideAttrs (oldAttrs: {
-      version = "0.11.6";
+      version = "0.11.14";
       
       src = super.fetchFromGitHub {
         owner = "abusch";
         repo = "nu_plugin_semver";
-        tag = "v0.11.6";
-        hash = "sha256-JF+aY7TW0NGo/E1eFVpBZQoxLxuGja8DSoJy4xgi1Lk=";
+        tag = "v0.11.14";
+        hash = "sha256-mfwgwY/iYdMz8Qn6a9zfpMHWHl2n1Q8ClkT+KiCAGyk=";
       };
       
-      cargoHash = "sha256-609w/7vmKcNv1zSfd+k6TTeU2lQuzHX3W5Y8EqKIiAM=";
+      cargoHash = "";
       
       meta = oldAttrs.meta // {
         platforms = super.lib.platforms.linux ++ super.lib.platforms.darwin;
