226ed88bf4a2cadaa92aeeb4cf9f79746684ec96
Nick Coleman <nick.coleman@educationperfect.com>
2026-02-09 16:18:21 +1300

    update nushell plugins

--- overlays/nushell-plugins.nix
+++ overlays/nushell-plugins.nix
@@ -2,16 +2,16 @@ self: super: {
   nushellPlugins = super.nushellPlugins // {
     secret = super.rustPlatform.buildRustPackage rec {
       pname = "nu_plugin_secret";
-      version = "0.3.0";
+      version = "0.6.0";
 
       src = super.fetchFromGitHub {
         owner = "nushell-works";
         repo = "nu_plugin_secret";
         rev = "v${version}";
-        hash = "sha256-Yv6xZcHf77VjbsKWaeWuaH85Bk8uf4bjrryXQzL76kM="; # Replace with actual hash
+        hash = "sha256-SO3MbIw/QVTYGsVY7HjsikkVHeJ4ObC5uHXcqRwAku4="; # Replace with actual hash
       };
 
-      cargoHash = "sha256-wqJZn9VwcOW9Pro1dWWqyIEb9LCj3B1EVfjxr0viKvs="; # Replace with actual hash
+      cargoHash = "sha256-EM2KohRalZXbmaKsPGo6Rz1GXOxBARlo2wFph1QMI0g="; # Replace with actual hash
 
       nativeBuildInputs = with super; [ pkg-config ];
       buildInputs = with super; [ openssl ];
